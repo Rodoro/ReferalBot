367c11c62778305f184c9d9c1fcbb19e4b7b54b8
Rodoro <86850777+Rodoro@users.noreply.github.com>
2025-06-27 21:21:52 +0300

    Fix adaptive

--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -1,14 +1,16 @@
+import { TypographyH2 } from '@/shared/ui/typography/TypographyH2';
+import { TypographyP } from '@/shared/ui/typography/TypographyP';
+import Header from '@/widgets/ui/layouts/Header/Header'
+
+export const metadata = { title: 'Главная' }
+
 export default function Home() {
   return (
     <>
-      <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        {/* TODO: Реализовать главную на которую будет вытаскиватся из всех проектов выжимка и также на ней будет реализована система тасков и всего подобного */}
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-        </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+      <Header breadcrumbs={[{ label: 'Главная', isCurrent: true }]} />
+      <main className="flex flex-1 flex-col pb-4 pt-0 px-8 max-w-7xl gap-4">
+        <TypographyH2 text='Главная' />
+        <TypographyP text='Чтобы перейти на нужную страницу, воспользуйтесь меню в левом верхнем углу страницы и выберите соответствующий пункт.' />
       </main>
     </>
   );
